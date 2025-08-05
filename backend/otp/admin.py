720d6af52b5531f2fffd68dab8ee48bd7ee0b26d
rayhan <prog.rayhan1@gmail.com>
2025-08-05 23:35:13 +0600

    feat: otp model created

--- backend/otp/admin.py
+++ backend/otp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import OTPModel
 
-# Register your models here.
+
+@admin.register(OTPModel)
+class OtpAdmin(admin.ModelAdmin):
+    list_display = (
+        "otp_for",
+        "identifier",
+        "reason",
+        "status",
+        "expires_at",
+    )
--- backend/otp/models.py
+++ backend/otp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from _applibs.model_choice_fields import OtpStatus
 
-# Create your models here.
+
+class OTPModel(models.Model):
+    otp_for = models.CharField(max_length=50)
+    identifier = models.CharField(max_length=100)
+    reason = models.CharField(max_length=60)
+    otp_code = models.CharField(max_length=20)
+    status = models.CharField(
+        max_length=20, 
+        choices=OtpStatus.choices, 
+        default=OtpStatus.INITIALIZE
+    )
+    expires_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    verified_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.otp_code
+    
+    class Meta:
+        verbose_name = "Otp"
+        verbose_name_plural = "Otps"
+        db_table = "otp"
+    
